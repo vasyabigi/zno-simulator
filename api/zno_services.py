0d535a555dd3da5d5938feeba98f9eefec96ccb7
Vasyl Stanislavchuk <vasyl.stanislavchuk@gmail.com>
2019-10-01 16:11:00 +0300

    Code review: use staticmethod.

--- api/zno_services.py
+++ api/zno_services.py
@@ -4,34 +4,23 @@ import random
 
 import requests
 
-
 IS_FETCH_REQUIRED = os.environ.get("IS_FETCH_REQUIRED", False)
-
 QUESTIONS_URL = (
     "https://much-better-adventures.s3.eu-central-1.amazonaws.com/questions.json"
 )
 
-print("LOADING QUESTIONS...")
-
-
-def load_questions():
-    if not IS_FETCH_REQUIRED:
-        with open("../questions.json", "r") as f:
-            return json.loads(f.read())
-
-    response = requests.get(QUESTIONS_URL)
-    return json.loads(response.content)
-
-
-QUESTIONS_FROM_SERVER = load_questions()
-QUESTIONS = [q["id"] for q in QUESTIONS_FROM_SERVER]
-QUESTIONS_MAP = {q["id"]: q for q in QUESTIONS_FROM_SERVER}
-
-print("LOADED QUESTIONS!")
-
 
 class QuestionsService:
     """Class designed for questions manipulations: load, check answers, etc."""
+
+    @staticmethod
+    def load_questions():
+        if not IS_FETCH_REQUIRED:
+            with open("../questions.json", "r") as f:
+                return json.loads(f.read())
+
+        response = requests.get(QUESTIONS_URL)
+        return json.loads(response.content)
 
     @staticmethod
     def load_random_question():
@@ -49,3 +38,8 @@ class QuestionsService:
             choice["id"] for choice in question["choices"] if choice["is_correct"]
         ]
         return sorted(correct_choices) == sorted(user_choices)
+
+
+QUESTIONS_FROM_SERVER = QuestionsService.load_questions()
+QUESTIONS = [q["id"] for q in QUESTIONS_FROM_SERVER]
+QUESTIONS_MAP = {q["id"]: q for q in QUESTIONS_FROM_SERVER}
