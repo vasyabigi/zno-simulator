702951cdb69466a58b99d45473abd9f105bf2d9f
lynxrv21 <dba5bbef@opayq.com>
2019-10-02 16:51:44 +0300

    Updates according to comments. Added API interaction functionality.

--- bot/config.py
+++ bot/config.py
@@ -1,7 +1,2 @@
 telegram_token = "746733366:AAEDtBc2cah8d6eg2ahleHKNJx0Yrv2sKUg"
-get_question_url = (
-    "http://zno-simulator-dev.eu-central-1.elasticbeanstalk.com/questions/random"
-)
-post_answer_url = (
-    "http://zno-simulator-dev.eu-central-1.elasticbeanstalk.com/questions/{id}/answers"
-)
+api_root = "http://zno-simulator-dev.eu-central-1.elasticbeanstalk.com"
