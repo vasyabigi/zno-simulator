0146d350b26f74fa31aa51f816d6bb83351c9893
Vasyl Stanislavchuk <vasyl.stanislavchuk@gmail.com>
2019-10-03 16:40:05 +0300

    Code review: simplify output strip.

--- content/converter.py
+++ content/converter.py
@@ -65,12 +65,7 @@ class QuestionConverter:
         """
         output = markdown.handle(cleaner.clean_html(str(soup)))
 
-        if output.startswith("\n\n"):
-            output = output[2:]
-
-        if output.endswith("\n\n"):
-            output = output[:-2]
-
+        output = output.strip("\n")
         # Hacky way to remove new lines but keep paragraphs
         output = output.replace("\n\n", "TEMP_BREAKER")
         output = output.replace("\n", " ")
