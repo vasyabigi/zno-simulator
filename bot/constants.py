88985fabc1ec495d9f4c4589336af402c7d10e2f
lynxrv21 <dba5bbef@opayq.com>
2019-10-09 20:56:28 +0300

    Improve retry logic, added log messages. Refactoring done.

---
+++ bot/constants.py
@@ -0,0 +1,26 @@
+from urllib.parse import urljoin
+
+import config
+
+QUESTION_URL = urljoin(config.api_root, '/questions/{id}')
+ANSWER_URL = urljoin(config.api_root, '/questions/{id}/answers')
+CHECK_MARK_BUTTON = '✅'
+CHECK_MARK_BLACK = '✔'
+CROSS_MARK = '❌'
+CROSS_MARK_BLACK = '✖'
+QUESTION_MARK = '❓'
+BOOK = '📖'
+INDEX_POINTING_RIGHT = '👉'
+YOUR_CHOICE_B = '*Ви обрали:*'
+CHOICES_AVAILABLE_B = '*Варіанти відповідей:*'
+
+START = 'старт'
+HELP = 'допомога'
+QUESTION = 'питання'
+GREETING_STR = f'Привіт! Напишіть *{QUESTION}* щоб отримати нове питання!'
+HELP_STR = f'Напишіть *{QUESTION}* щоб отримати нове питання.'
+EXPLANATION_STR = f'{BOOK} Пояснення'
+QUESTION_BOOKS = '📚'
+FACEPALM = '🤦‍♂️'
+SORRY_ERROR = f'{FACEPALM} Виникла помилка... Спробуйте ще раз, ми вже прикладаємо подорожник.'
+SHOW_ANSWER = f'{BOOK} Показати правильну відповідь'
