57b4843598f3613668c356af98578edda2f234e3
SashaTsios <mr.tsios@gmail.com>
2020-02-06 20:40:42 +0200

    landing page start w black

--- landing_page_service/core/views.py
+++ landing_page_service/core/views.py
@@ -6,4 +6,4 @@ from core.models import ZNOSubject
 def landing_page_ZNOtask(request):
     subjects = ZNOSubject.objects.filter(is_active_subject=True)
 
-    return render(request, "ZNOtask.html", context={"subjects": subjects, },)
+    return render(request, "ZNOtask.html", context={"subjects": subjects,},)
