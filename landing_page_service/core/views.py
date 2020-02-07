ddae498eb2b58a0a74cea12d0081198a00498861
SashaTsios <mr.tsios@gmail.com>
2020-02-07 15:16:18 +0200

    second minor changes and about-us page

--- landing_page_service/core/views.py
+++ landing_page_service/core/views.py
@@ -7,3 +7,8 @@ def landing_page_view(request):
     subjects = ZNOSubject.objects.filter(is_active=True)
 
     return render(request, "landing_page.html", context={"subjects": subjects,},)
+
+
+def about_us_view(request):
+    variable = []
+    return render(request, "about_us.html", context={"about": variable,},)
--- landing_page_service/landing_page/urls.py
+++ landing_page_service/landing_page/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import landing_page_view
+from core.views import landing_page_view, about_us_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", landing_page_view),
+    path("about-us/", about_us_view),
 ]
