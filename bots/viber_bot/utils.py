88cebdab4b381d431c951ce90a2ce78e5e042fd4
andriinovak <andrii.novak91@gmail.com>
2020-02-08 11:34:03 +0200

    fix utils.py

--- bots/viber_bot/utils.py
+++ bots/viber_bot/utils.py
@@ -2,11 +2,11 @@ from requests import get
 import json
 
 URL_WITH_QUESTION = (
-    "http://zno-dev.eu-central-1.elasticbeanstalk.com/questions/random?subject=ukr"
+    "http://zno-dev.eu-central-1.elasticbeanstalk.com/questions/random?subject="
 )
 
 
-def get_question():
-    get_all_page = get(URL_WITH_QUESTION)
+def get_question(predmet):
+    get_all_page = get(URL_WITH_QUESTION + predmet)
     page_text = json.loads(get_all_page.content)
     return page_text
