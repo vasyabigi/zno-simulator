d51b93a69ce19fedc2d5d375a63aa0251b570c4e
lynxrv21 <dba5bbef@opayq.com>
2020-02-18 16:57:55 +0200

    Update formatter to convert response depending on `format` url parameter.

--- api_service/serializers.py
+++ api_service/serializers.py
@@ -1,13 +1,39 @@
-def serialize_question(question_data):
+from enum import Enum
+
+import markdown2
+
+
+class Formats(Enum):
+    HTML = "html"
+    RAW = "raw"
+    MARKDOWN = "markdown"
+
+
+def markdown_to_html(text):
+    return markdown2.markdown(text)
+
+
+def markdown_to_raw(text):
+    # TODO: improve method to deal with all markdown characters
+    return text.replace("*", "")
+
+
+def serialize_question(question_data, serialization_format):
+    formatter = {
+        Formats.HTML.value: markdown_to_html,
+        Formats.MARKDOWN.value: lambda x: x,
+        Formats.RAW.value: markdown_to_raw,
+    }.get(serialization_format)
+
     return {
         "id": question_data["id"],
-        "content": question_data["content"],
+        "content": formatter(question_data["content"]),
         "image": question_data["image"],
-        "explanation": question_data["explanation"],
+        "explanation": formatter(question_data["explanation"]),
         "choices": [
             {
                 "id": choice["id"],
-                "content": choice["content"],
+                "content": formatter(choice["content"]),
                 "is_correct": choice["is_correct"],
             }
             for choice in question_data["choices"]
