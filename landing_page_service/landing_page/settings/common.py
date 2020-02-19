eace25fab97d527e4003795eaf132245060d057a
Vasyl Stanislavchuk <vasyl.stanislavchuk@gmail.com>
2020-02-19 11:33:18 +0200

    Separated settings to support different DB drivers.

--- landing_page_service/landing_page/settings/common.py
+++ landing_page_service/landing_page/settings/common.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -73,17 +73,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "landing_page.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -115,6 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
---
+++ landing_page_service/landing_page/settings/prod.py
@@ -0,0 +1,30 @@
+import os
+
+from django.core.exceptions import ImproperlyConfigured
+
+from .common import *  # noqa F401
+
+
+def get_env_variable(var_name):
+    """
+    Get the environment variable or return exception.
+
+    """
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s env variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": get_env_variable("DATABASE_NAME"),
+        "USER": get_env_variable("DATABASE_USER"),
+        "PASSWORD": get_env_variable("DATABASE_PASSWORD"),
+        "HOST": get_env_variable("DATABASE_HOST"),
+        "PORT": "5432",
+        "CONN_MAX_AGE": 0,
+    }
+}
