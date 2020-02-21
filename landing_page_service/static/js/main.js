3d1faf4f30995e9f09fb687e69203a8ea47e36a9
ZuluSpirit <serz@ukr.net>
2020-02-21 23:49:45 +0200

    Add Front-end fix

--- landing_page_service/static/js/main.js
+++ landing_page_service/static/js/main.js
@@ -15,27 +15,4 @@ $(document).ready(function () {
     $("#toTop").click(function () {
         $("html, body").animate({ scrollTop: 0 }, 800);
     });
-
-    /*btn header hover*/
-    // var $btnPr = $('#btn-primary');
-    // var $btnSec = $('#btn-secondary');
-
-    // $btnSec.mouseover(function () {
-    //     $btnPr.addClass('btn--transparent');
-    //     $(this).removeClass('btn--transparent');
-    // });
-
-    // $btnSec.mouseout(function () {
-    //     $(this).addClass('btn--transparent');
-    //     $btnPr.removeClass('btn--transparent');
-    // });
-
-    /*mobile header btn*/
-    var containerWidth = $("body").width();
-
-    if (containerWidth <= 768) {
-        $btnPr.removeClass('btn--big');
-        $btnSec.removeClass('btn--big');
-    }
-
 });
